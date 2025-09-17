a7dd919d73bc52b4cf35214bec3791f5014aa54b
Stepanova <stepanovaad@mer.ci.nsu.ru>
2025-09-17 16:25:43 +0700

    students

---
+++ src/app/api/students/route.ts
@@ -0,0 +1,11 @@
+import { getStudentDb } from '@/db/studentsDb';
+
+export async function GET(): Promise<Response> {
+  const students = await getStudentDb();
+
+  return new Response(JSON.stringify(students), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
