6591a0c40a2800803963fc66ee20d65f9631d457
Stepanova <stepanovaad@mer.ci.nsu.ru>
2025-10-15 16:17:28 +0700

    StepanovaAddEnd

--- src/components/Students/Students.tsx
+++ src/components/Students/Students.tsx
@@ -4,9 +4,10 @@ import useStudents from '@/hooks/useStudents';
 import type StudentInterface from '@/types/StudentInterface';
 import styles from './Students.module.scss';
 import Student from './Student/Student';
+import AddStudent from './AddStudent/AddStudent';
 
 const Students = (): React.ReactElement => {
-  const { students, deleteStudentMutate } = useStudents();
+  const { students, deleteStudentMutate, createStudentMutate } = useStudents();
 
   const onDeleteHandler = (studentId: number): void => {
     if(confirm('Удалить студента?')){
@@ -14,15 +15,29 @@ const Students = (): React.ReactElement => {
     }
   }
 
+const onAddStudentHandler = async (student: Omit<StudentInterface, 'id'>): Promise<void> => {
+    await createStudentMutate(student);
+  };
+
   return (
     <div className={styles.Students}>
-      {students.map((student: StudentInterface) => (
-        <Student
-          key={student.id}
-          student={student}
-          onDelete={onDeleteHandler}
-/>
-      ))}
+      <AddStudent onAddStudent={onAddStudentHandler} />
+      
+      <h3>Список студентов</h3>
+      
+      {!students ? (
+        <p>Загрузка...</p>
+      ) : students.length === 0 ? (
+        <p>Студентов нет</p>
+      ) : (
+        students.map((student: StudentInterface) => (
+          <Student
+            key={student.id}
+            student={student}
+            onDelete={onDeleteHandler}
+          />
+        ))
+      )}
     </div>
   );
 };
